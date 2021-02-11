e2e86e3a468bdca5c926d58318f33472657c97a1
Muneer Sial <muneer.sial2354@gmail.com>
2021-02-12 04:01:09 +0500

    express and middleware

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handle);
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    //res.setHeader('Content-Type', 'text/html') --optional
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
