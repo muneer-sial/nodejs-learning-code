c1d374da371afa780a20a8379576f3f84ca06519
Muneer Sial <muneer.sial2354@gmail.com>
2021-02-13 05:43:37 +0500

    rendering static assets

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParse.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'error404.html'))
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(3000);
